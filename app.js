0c90b8d19f86dc3e61dd245d08eca8b979c2eaed
Arwen Qin <arwenqin626@gmail.com>
2023-08-08 23:40:41 +0800

    tried twice

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import "dotenv/config";
 import session from "express-session";
 import AuthController from "./users/auth-controller.js";
 
-const allowedOrigins = ['https://a5--harmonious-gnome-362675.netlify.app/', process.env.FRONTEND_URL];
+const allowedOrigins = ['https://a5--harmonious-gnome-362675.netlify.app', process.env.FRONTEND_URL];
 const app = express();
 app.use(
     cors({
