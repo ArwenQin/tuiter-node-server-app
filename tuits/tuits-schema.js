eba3e48965006ac5936dee3eef904ff46968c8ba
Arwen Qin <arwenqin626@gmail.com>
2023-08-15 21:14:40 +0800

    fixed schema

--- tuits/tuits-schema.js
+++ tuits/tuits-schema.js
@@ -3,6 +3,16 @@ const schema = mongoose.Schema({
   tuit: String,
   likes: Number,
   liked: Boolean,
+  disliked: Boolean,
+  dislikes: Number,
+  replies: Number,
+  retuits: Number,
+  time: String,
+  title: String,
+  handle: String,
+  topic: String,
+  image: String,
+  username: String,
 }, {collection: 'tuits'});
 export default schema;
 
